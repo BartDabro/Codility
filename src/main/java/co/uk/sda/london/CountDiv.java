c5a151e82ace76288912f2d7f71fda0eb9d0739a
Bart Dabrowski <bart.dabrowski.uk@gmail.com>
2019-12-14 12:57:05 +0000

    resolve exercise CountDiv

--- src/main/java/co/uk/sda/london/CountDiv.java
+++ src/main/java/co/uk/sda/london/CountDiv.java
@@ -1,4 +1,27 @@
-package co.uk.london.oca;
+package co.uk.sda.london;
+
+
+/*
+
+Write a function:
+
+class Solution { public int solution(int A, int B, int K); }
+
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+        { i : A ≤ i ≤ B, i mod K = 0 }
+
+        For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+        Write an efficient algorithm for the following assumptions:
+
+        A and B are integers within the range [0..2,000,000,000];
+        K is an integer within the range [1..2,000,000,000];
+        A ≤ B.
+
+*/
+
+
 
 public class CountDiv {
 
@@ -16,7 +39,58 @@ public class CountDiv {
             {
                 if(B==A)
                 {
-                    return 0;
+
+                    if(K>A) {
+                        if(A%K==0)
+                        {
+                            if(A==0)
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                        else
+                        {
+                            if(A!=0)
+                            {
+                                return 0;
+                            }
+                            else
+                            {
+                                return 1;
+                            }
+                        }
+
+
+                    }
+                    else{
+                        if(A%K==0)
+                        {
+                            if(A==0)
+                            {
+                                return 0;
+                            }
+                            else
+                            {
+                                return 1;
+                            }
+                        }
+                        else
+                        {
+                            if(A!=0)
+                            {
+                                return 0;
+                            }
+                            else
+                            {
+                                return 1;
+                            }
+                        }
+                    }
+
                 }
                 else
                 {
@@ -24,14 +98,23 @@ public class CountDiv {
                     int div = diff/K;
                     if(K>A)
                     {
-                        div++;
+                        if(A==0)
+                        {
+                            return (B/K+1);
+                        }
+                        else{
+                            if(K>2*A)
+                            {
+                                return div;
+                            }
+                            div++;
+                        }
                     }
                     if(B%K==0)
                     {
                         div++;
-
                     }
-                    if(A%K==0)
+                    else if(A%K==0)
                     {
                         div++;
                     }
@@ -44,7 +127,8 @@ public class CountDiv {
                 }
             }
         }
-        return 1;
+        return 0;
+
     }
 }
 
